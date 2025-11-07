be110afa693a8d6e657af290e967aa11ef613916
khaled <akhaledahmedmahamed@gmail.com>
2025-11-07 11:11:29 +0200

    Fix eslint warning for email validation

--- client/src/utils/validation.js
+++ client/src/utils/validation.js
@@ -9,6 +9,7 @@
  * @returns {boolean} True if valid
  */
 export const isValidEmail = (email) => {
+  // eslint-disable-next-line no-useless-escape
   const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
   return emailRegex.test(email);
 };
